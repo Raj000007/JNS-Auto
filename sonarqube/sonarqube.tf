0efc9db74006ddacc1a03fc40db9f6db8056212a
automateopsprojects <120359592+automateopsprojects@users.noreply.github.com>
2023-07-25 09:42:37 +0530

    Update sonarqube.tf

--- sonarqube/sonarqube.tf
+++ sonarqube/sonarqube.tf
@@ -6,18 +6,18 @@ terraform {
   }
 }
 provider "aws" {
-  access_key = "AKIA4IMB4V72RSWVU77F"
-  secret_key = "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"
+  access_key = "AKIAU4ZRQUPGXUDNUPUQ"
+  secret_key = "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"
   region     = "us-east-1"
 }
 
 resource "aws_instance" "sonarqube" {
   ami                    = "ami-0261755bbcb8c4a84"
   instance_type          = "t2.medium"
-  key_name               = "jenkiskey"
-  vpc_security_group_ids = ["${aws_security_group.sonarqube.id}"]
+  key_name               = "skillrary1234"
+  vpc_security_group_ids = ["${aws_security_group.sonarqube_sg.id}"]
   user_data              = file("sonar.sh")
   tags = {
-    "Name" = "Jenkins"
+    "Name" = "SonarQube"
   }
 }
