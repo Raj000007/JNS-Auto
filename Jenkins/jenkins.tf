ce7537ea443a2224c8ea3c0a094e9dc129e6a482
automateopsprojects <120359592+automateopsprojects@users.noreply.github.com>
2023-07-25 10:03:14 +0530

    Update jenkins.tf

--- Jenkins/jenkins.tf
+++ Jenkins/jenkins.tf
@@ -6,16 +6,16 @@ terraform {
   }
 }
 provider "aws" {
-  access_key = "AKIA4IMB4V72RSWVU77F"
-  secret_key = "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"
+  access_key = "AKIAU4ZRQUPGXUDNUPUQ"
+  secret_key = "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"
   region     = "us-east-1"
 }
 
 resource "aws_instance" "jenkins" {
   ami                    = "ami-0261755bbcb8c4a84"
   instance_type          = "t2.medium"
-  key_name               = "jenkiskey"
-  vpc_security_group_ids = ["${aws_security_group.jenkins.id}"]
+  key_name               = "skillrary1234"
+  vpc_security_group_ids = ["${aws_security_group.jenkins_sg.id}"]
   provisioner "remote-exec" {
     inline = [
       "sudo curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null",
@@ -25,13 +25,17 @@ resource "aws_instance" "jenkins" {
       "sudo apt-get install jenkins -y",
       "sudo apt update",
       "sudo cat /var/lib/jenkins/secrets/initialAdminPassword",
+      "sudo apt update -y",
+      "sudo apt install openjdk-8-jdk -y",
+      "sudo -i",
+      "ls /usr/lib/jvm",
     ]
   }
   connection {
     type        = "ssh"
     host        = self.public_ip
     user        = "ubuntu"
-    private_key = file("C:\\Users\\User\\Downloads\\jenkiskey.pem")
+    private_key = file("C:\\Users\\User\\Downloads\\skillrary1234.pem")
   }
   tags = {
     "Name" = "Jenkins"
