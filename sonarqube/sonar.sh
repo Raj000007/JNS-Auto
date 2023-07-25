1ea562d0c74248740838f2e77619c664e0122f77
automateopsprojects <120359592+automateopsprojects@users.noreply.github.com>
2023-07-25 09:47:03 +0530

    Update sonar.sh

--- sonarqube/sonar.sh
+++ sonarqube/sonar.sh
@@ -1,77 +1,119 @@
 #!/bin/bash
+sudo cp /etc/sysctl.conf /root/sysctl.conf_backup
+sudo cat <<EOT> /etc/sysctl.conf
+vm.max_map_count=262144
+fs.file-max=65536
+ulimit -n 65536
+ulimit -u 4096
+EOT
+sudo cp /etc/security/limits.conf /root/sec_limit.conf_backup
+sudo cat <<EOT> /etc/security/limits.conf
+sonarqube   -   nofile   65536
+sonarqube   -   nproc    409
+EOT
 
-# Update system and install OpenJDK 11
+sudo apt-get update -y
+sudo apt-get install openjdk-11-jdk -y
+sudo update-alternatives --config java
+
+java -version
+
 sudo apt update
-sudo apt install -y openjdk-11-jdk
+wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | sudo apt-key add -
 
-# Adjust system limits
-sudo sysctl -w vm.max_map_count=262144
-sudo sysctl -w fs.file-max=131072
-ulimit -n 131072
-ulimit -u 8192
-
-
-
-# Wait for the system to reboot and proceed to the next commands
-
-# Create sonarh2s user
-sudo adduser --system --no-create-home --group --disabled-login sonarh2s
-
-# Install PostgreSQL 13
-wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O- | sudo apt-key add -
-echo "deb [arch=amd64] http://apt.postgresql.org/pub/repos/apt/ focal-pgdg main" | sudo tee /etc/apt/sources.list.d/postgresql.list
-sudo apt update
-sudo apt install -y postgresql-13
-
-
-# Set password for postgres user
-sudo echo "postgres:admin" | sudo chpasswd
-sudo -u postgres createuser sonaruser
-sudo -u postgres psql -c "ALTER USER sonaruser WITH ENCRYPTED PASSWORD 'admin';"
-sudo -u postgres psql -c "CREATE DATABASE sonardb OWNER sonaruser;"
-
-# Download and install SonarQube
-wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-8.9.10.61524.zip
-sudo apt -y install unzip
-sudo unzip sonarqube-*.zip -d /opt
-sudo mv /opt/sonarqube-* /opt/sonarqube
-sudo chown -R sonarh2s:sonarh2s /opt/sonarqube
-
-# Configure SonarQube properties
-sudo tee /opt/sonarqube/conf/sonar.properties <<EOF
-sonar.jdbc.username=sonaruser
-sonar.jdbc.password=admin
-sonar.jdbc.url=jdbc:postgresql://localhost/sonardb
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
+sudo apt install postgresql postgresql-contrib -y
+#sudo -u postgres psql -c "SELECT version();"
+sudo systemctl enable postgresql.service
+sudo systemctl start  postgresql.service
+sudo echo "postgres:admin123" | chpasswd
+runuser -l postgres -c "createuser sonar"
+sudo -i -u postgres psql -c "ALTER USER sonar WITH ENCRYPTED PASSWORD 'admin123';"
+sudo -i -u postgres psql -c "CREATE DATABASE sonarqube OWNER sonar;"
+sudo -i -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE sonarqube to sonar;"
+systemctl restart  postgresql
+#systemctl status -l   postgresql
+netstat -tulpena | grep postgres
+sudo mkdir -p /sonarqube/
+cd /sonarqube/
+sudo curl -O https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-8.3.0.34182.zip
+sudo apt-get install zip -y
+sudo unzip -o sonarqube-8.3.0.34182.zip -d /opt/
+sudo mv /opt/sonarqube-8.3.0.34182/ /opt/sonarqube
+sudo groupadd sonar
+sudo useradd -c "SonarQube - User" -d /opt/sonarqube/ -g sonar sonar
+sudo chown sonar:sonar /opt/sonarqube/ -R
+cp /opt/sonarqube/conf/sonar.properties /root/sonar.properties_backup
+cat <<EOT> /opt/sonarqube/conf/sonar.properties
+sonar.jdbc.username=sonar
+sonar.jdbc.password=admin123
+sonar.jdbc.url=jdbc:postgresql://localhost/sonarqube
+sonar.web.host=0.0.0.0
+sonar.web.port=9000
 sonar.web.javaAdditionalOpts=-server
-sonar.java.binaries=target/classes
-EOF
+sonar.search.javaOpts=-Xmx512m -Xms512m -XX:+HeapDumpOnOutOfMemoryError
+sonar.log.level=INFO
+sonar.path.logs=logs
+EOT
 
-# Create SonarQube service file
-sudo tee /etc/systemd/system/sonar.service <<EOF
+sudo cat <<EOT> /etc/systemd/system/sonarqube.service
 [Unit]
 Description=SonarQube service
 After=syslog.target network.target
 
 [Service]
 Type=forking
+
 ExecStart=/opt/sonarqube/bin/linux-x86-64/sonar.sh start
 ExecStop=/opt/sonarqube/bin/linux-x86-64/sonar.sh stop
+
+User=sonar
+Group=sonar
+Restart=always
+
 LimitNOFILE=65536
 LimitNPROC=4096
-User=sonarh2s
-Group=sonarh2s
-Restart=on-failure
+
 
 [Install]
 WantedBy=multi-user.target
-EOF
+EOT
 
-# Reload systemd configuration and start SonarQube service
 sudo systemctl daemon-reload
-sudo systemctl enable sonar
-sudo systemctl start sonar
+sudo systemctl enable sonarqube.service
+#systemctl start sonarqube.service
+#systemctl status -l sonarqube.service
+sudo apt-get install nginx -y
+rm -rf /etc/nginx/sites-enabled/default
+rm -rf /etc/nginx/sites-available/default
+sudo cat <<EOT> /etc/nginx/sites-available/sonarqube
+server{
+    listen      80;
+    server_name sonarqube.groophy.in;
 
+    access_log  /var/log/nginx/sonar.access.log;
+    error_log   /var/log/nginx/sonar.error.log;
 
-# Check SonarQube status as sonarh2s user
-sudo -Hu sonarh2s /opt/sonarqube/bin/linux-x86-64/sonar.sh start
-sudo -Hu sonarh2s /opt/sonarqube/bin/linux-x86-64/sonar.sh console
+    proxy_buffers 16 64k;
+    proxy_buffer_size 128k;
+
+    location / {
+        proxy_pass  http://127.0.0.1:9000;
+        proxy_next_upstream error timeout invalid_header http_500 http_502 http_503 http_504;
+        proxy_redirect off;
+              
+        proxy_set_header    Host            \$host;
+        proxy_set_header    X-Real-IP       \$remote_addr;
+        proxy_set_header    X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header    X-Forwarded-Proto http;
+    }
+}
+EOT
+ln -s /etc/nginx/sites-available/sonarqube /etc/nginx/sites-enabled/sonarqube
+systemctl enable nginx.service
+#systemctl restart nginx.service
+sudo ufw allow 80,9000,9001/tcp
+
+echo "System reboot in 30 sec"
+sleep 30
+reboot
